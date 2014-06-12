735ed825a3ceb2d93ecd38ade9dd63059e53d816
gratefulDead420 <tasmith1990@gmail.com>
2014-06-11 22:27:44 -0400

    Create forum.php

---
+++ forum.php
@@ -0,0 +1,53 @@
+<?php
+
+session_start
+
+/*
+* script: forum_main.php
+* @developed by gratefulDeadty
+*/
+
+$forums = new Forum($dbh); //calls Forum class.
+
+$forum_main = $forums->getForum();
+
+if (empty($_GET['action']))
+{
+	//create a foreach to gather our fetch array.
+	foreach ($forum_main as $forum)
+	{
+		$forum_name = htmlentities($forum['name']); //name
+		$forum_desc = htmlentities($forum['description']); //description
+		echo '<a href="forum.php?forum='.$forum_name.'">' .$forum_name . ' - ' . $forum_desc . '<br />';
+	}
+}
+
+if ($_GET['forum'])
+{
+	foreach ($forum_topics as $topics)
+	{
+		//first we check to see if there are any topics created.
+		$stmt = $dbh->prepare('SELECT * FROM topics WHERE ' . 'forum=:forum ');
+		$stmt->bindParam('forum', $_GET['forum']);
+		$stmt->execute();
+		$count = $stmt->rowCount();
+		if ($count == 0)
+		{
+			echo '<p>No topics have been found under '.htmlspecialchars($_GET['forum'],ENT_QUOTES).'</p>';
+			echo '<br /><br /><a href="addtopic.php?forum='.$_GET['forum'].'">Add Topic</a>';
+		}
+		else
+		{
+			$topic_id = htmlentities($topics['id']); //id of topic.
+			$topic_title = htmlentities($topics['title']); //topic title.
+			$topic_starter = htmlentities($topics['starter']); //topic starter.
+			$topic_lastreply = htmlentities($topics['lastreply']); //last replied user
+			echo '<a href="topic.php?id='.$topic_id.'">' .$topic_title . ' - ' . $forum_desc . '<br />';
+
+		} //end else
+
+	} //end foreach()
+
+} //end $_GET
+
+?>
