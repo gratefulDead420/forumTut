549a56770345e436f8c40fe3f413a673ce9ade7d
gratefulDead420 <tasmith1990@gmail.com>
2014-06-14 02:32:25 -0400

    Update forum.php

--- forum.php
+++ forum.php
@@ -1,11 +1,9 @@
 <?php
 
-
 /*
 * script: forum_main.php
 * @developed by gratefulDeadty 2014
 */
-
 
 require 'init.php'; //our script which begins the forum & database connection.
 
@@ -13,6 +11,19 @@ $forum_main = $forums->getForum();
 
 if (empty($_GET['forum']))
 {
+
+    ?>
+<div align="center">
+<table>
+<tr>
+<td width="100">Forum</td>
+<td width="200"></td>
+<td width="50">Topics</td>
+<td width="50">Posts</td>
+<td width="200">Last Post</td>
+</tr>
+</div>
+        <?php
         $stmt = $dbh->query('SELECT * FROM forum_main');
         $stmt->execute();
         $count = $stmt->rowCount();
@@ -20,23 +31,22 @@ if (empty($_GET['forum']))
         {
             echo 'No forums created yet!';
         }
-        
+
 	//create a foreach to gather our fetch array.
 	foreach ($forum_main as $forum)
 	{
 		$forum_name = $forum['name']; //name
 		$forum_desc = $forum['description']; //description
                 $forumid = $forum['id'];
-		echo '<a href="forum.php?forum='.$forumid.'">' .$forum_name . '</a> - <i>' . $forum_desc . '</i><br />';
+		echo '<tr><td><a href="forum.php?forum='.$forumid.'">' .$forum_name . '</a></td><td><i>' . $forum_desc . '</i></td><td>1</td><td>1</div></td><td><i>last post by</i></td>';
 
 	} //end foreach()
-
+        echo '</tr></table>';
 } //end if()
 
 
 if ($_GET['forum'])
 {
-
 	//first we check to see if there are any topics created.
 	$stmt = $dbh->prepare('SELECT id,title,starter,lastreply FROM topics WHERE ' . 'forum=:forum ');
 	$stmt->bindParam('forum', $_GET['forum']);
