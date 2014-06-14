5ab831eeb40f4bd94c7ec200806ef94efe3d57c6
gratefulDead420 <tasmith1990@gmail.com>
2014-06-13 23:08:54 -0400

    Update forum.php

--- forum.php
+++ forum.php
@@ -7,51 +7,62 @@
 */
 
 
-require_once = 'init.php'; //our script which begins the forum & database connection.
-
-
-
-$forums = new Forum($dbh); //calls Forum class.
+require 'init.php'; //our script which begins the forum & database connection.
 
 $forum_main = $forums->getForum();
 
-if (empty($_GET['action']))
+if (empty($_GET['forum']))
 {
+        $stmt = $dbh->query('SELECT * FROM forum_main');
+        $stmt->execute();
+        $count = $stmt->rowCount();
+        if ($count == 0)
+        {
+            echo 'No forums created yet!';
+        }
+        
 	//create a foreach to gather our fetch array.
 	foreach ($forum_main as $forum)
 	{
-		$forum_name = htmlentities($forum['name']); //name
-		$forum_desc = htmlentities($forum['description']); //description
-		echo '<a href="forum.php?forum='.$forum_name.'">' .$forum_name . ' - ' . $forum_desc . '<br />';
-	}
-}
-
-if ($_GET['forum'])
-{
-	foreach ($forum_topics as $topics)
-	{
-		//first we check to see if there are any topics created.
-		$stmt = $dbh->prepare('SELECT * FROM topics WHERE ' . 'forum=:forum ');
-		$stmt->bindParam('forum', $_GET['forum']);
-		$stmt->execute();
-		$count = $stmt->rowCount();
-		if ($count == 0)
-		{
-			echo '<p>No topics have been found under the '.htmlspecialchars($_GET['forum'],ENT_QUOTES).' forum.</p>';
-			echo '<br /><br /><a href="addtopic.php?forum='.htmlspecialchars($_GET['forum'],ENT_QUOTES).'">Add Topic</a>';
-		}
-		else
-		{
-			$topic_id = htmlentities($topics['id']); //id of topic.
-			$topic_title = htmlentities($topics['title']); //topic title.
-			$topic_starter = htmlentities($topics['starter']); //topic starter.
-			$topic_lastreply = htmlentities($topics['lastreply']); //last replied user
-			echo '<a href="topic.php?id='.$topic_id.'">' .$topic_title . ' - ' . $forum_desc . '<br />';
-
-		} //end else
+		$forum_name = $forum['name']; //name
+		$forum_desc = $forum['description']; //description
+                $forumid = $forum['id'];
+		echo '<a href="forum.php?forum='.$forumid.'">' .$forum_name . '</a> - <i>' . $forum_desc . '</i><br />';
 
 	} //end foreach()
 
-} //end $_GET
+} //end if()
+
+
+if ($_GET['forum'])
+{
+
+	//first we check to see if there are any topics created.
+	$stmt = $dbh->prepare('SELECT id,title,starter,lastreply FROM topics WHERE ' . 'forum=:forum ');
+	$stmt->bindParam('forum', $_GET['forum']);
+	$stmt->execute();
+	$count = $stmt->rowCount();
+	if ($count == 0)
+	{
+	        echo '<p>No topics have been found under the '.htmlspecialchars($_GET['forum'],ENT_QUOTES).'</a> forum.</p>';
+		echo '<br /><br /><a href="addtopic.php?forum='.htmlspecialchars($_GET['forum'],ENT_QUOTES).'">Add Topic</a>';
+	}
+	else
+	{
+        	foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $topics)
+                {
+			$topic_id = $topics['id']; //id of topic.
+			$topic_title = $topics['title']; //topic title.
+			$topic_starter = $topics['starter']; //topic starter.
+			$topic_lastreply = $topics['lastreply']; //last replied user
+			echo '<a href="topic.php?id='.$topic_id.'">' .$topic_title . '<br />';
+
+                } //end foreach()
+
+                echo '<br /><br /><a href="addtopic.php?forum='.htmlspecialchars($_GET['forum'],ENT_QUOTES).'">Add Topic</a>';
+
+	} //end else()
+
+} //end if()
 
 ?>
