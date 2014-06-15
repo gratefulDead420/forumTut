8bc6e880d0181d777dedfe78d8fbbdcbeb19c2f7
gratefulDead420 <tasmith1990@gmail.com>
2014-06-15 15:26:23 -0400

    Update topic.php

--- topic.php
+++ topic.php
@@ -8,73 +8,74 @@
 require 'init.php';
 
 $topic = $forums->topicData($_GET['id']);
-
-echo '<center><strong>Topic '.$topic['id'].' - '.$topic['title'].'</strong></center>';
+$reply = $forums->replyData($_GET['id']);
 
 if(empty($_GET['id']) === true)
 {
-	$errors[] = 'Error: Forum does not exist.';
+	$errors[] = 'Error: Forum does not exist. ';
 }
 else
 {
-	$stmt = $dbh->prepare('SELECT * FROM topics WHERE ' . 'id=:id ');
-	$stmt->bindParam('id', $_GET['id']);
-	$stmt->execute();
-	$count = $stmt->rowCount();
-	if ($count == 0)
+        if (count($topic['id']) == 0)
 	{
-		$errors[] = 'Error: This topic does not exist.';
+		$errors[] = 'Error: This topic doesn\'t exist. Click <a href="forum.php">here</a> to go back.';
 	}
 
-	//success -> now we display the topic.
+	//success -> now we display the topic, but only if no errors are involved.
 	if(empty($errors) === true)
 	{
 		$starter = $topic['starter'];
 		$title = $topic['title'];
 		$message = $topic['message'];
-		echo ' ' .$starter. ' - ' .$message. '<br /><br />';
+                $created = $topic['created'];
+                echo '<p><strong>'.$title.'</strong> - <span style="font-size:8pt;"><i>Posted by '.$starter.'</i></span><br />'.$message.'</p>';
 
-	} //end empty($errors)
+		$i = 0;
+    		foreach ($reply as $replies)
+    		{
+			++$i;
+            		$reply_username = $replies['username'];
+	    		$reply_message = $replies['message'];
+            		$created = $replies['created'];
+            		$whatforum = $replies['forum'];
+	    		echo ''.$i.': ' .$reply_username. ' - ' .$reply_message. '<br /><br />';
+    		}
 
-    //begin Reply section.
-    $stmt = $dbh->prepare('SELECT * FROM replies WHERE ' . 'topicid=:topicid ');
-    $stmt->bindParam('topicid', $_GET['id']);
-    $stmt->execute();
-    foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $reply)
-    {
-    	$reply_username = $reply['username'];
-	$reply_message = $reply['message'];
-	echo ' ' .$reply_username. ' - ' .$reply_message. '<br />';
-    }
+		echo '<br /><div><strong>Reply:</strong><form method="POST">Email/Name: <input type="text" name="username"><br /><br /><textarea name="message" cols="40" rows="4">Body</textarea>
+<br /><input type="hidden" name="topicid" value="'.htmlspecialchars($_GET['id'],ENT_QUOTES).'"><input type="submit" name="submit" value="Add Reply"></form></div>';
 
-	echo '<br /><div><strong>Reply:</strong><form method="POST">Email/Name: <input type="text" name="username"><br /><textarea name="message" cols="40" rows="4">Body</textarea>
-<br /><input type="hidden" name="topicid" value="'.$_GET['id'].'"><input type="submit" name="submit" value="Add Reply"></form></div>';
+	}
 
 }
 
 if (isset($_POST['submit']))
 {
-    //submitting the reply.
+        //submitting the reply.
 	if (empty($_POST['message']) || empty($_POST['username']) === true)
 	{
 	    $errors[] = 'Error: You must enter a reply message and/or an email.';
 	}
 	else
 	{
-	    	//insert the reply into the database.
-		$message = $_POST['message'];
-		$username = $_POST['username'];
-		$topicid = $_POST['topicid'];
-		$insert_reply = $forums->addReply($message,$username,$topicid);
-	    	echo 'You have created a reply!';
+	        //insert the reply into the database.
+		$message = htmlentities($_POST['message']);
+		$username = htmlentities($_POST['username']);
+		$topicid = htmlentities($_POST['topicid']);
+                $whatforum = htmlentities($topic['forum']);
+                $created = date('Y-m-d H:i:s');
+		$insert_reply = $forums->addReply($message,$username,$topicid,$whatforum,$created);
+                $stmt = $dbh->prepare('UPDATE forum_main SET ' . 'lastpost=:lastpost WHERE ' . 'id=:id ');
+                $stmt->bindParam('lastpost', $username);
+                $stmt->bindParam('id', $whatforum);
+                $stmt->execute();
+	        echo 'You have created a reply!';
 	}
 
 }
 
-    	//displaying all errors from the $errors[] array.
-	if (empty($errors) === false)
-	{
-    	echo '<p>' . implode('</p><p>', $errors) . '</p>';	
-    }
-
+//if any errors occured, we'll display them using the $errors[] array.
+if (empty($errors) === false)
+{
+	echo ' '.implode($errors).' ';
+}
 ?>
