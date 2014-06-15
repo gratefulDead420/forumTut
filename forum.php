0c01521da816fed85bce2f36ceee6aded756fe73
gratefulDead420 <tasmith1990@gmail.com>
2014-06-15 15:59:38 -0400

    Update forum.php

--- forum.php
+++ forum.php
@@ -8,21 +8,24 @@
 require 'init.php'; //our script which begins the forum & database connection.
 
 $forum_main = $forums->getForum();
+$topics = $forums->getTopics($_GET['forum']);
 
 if (empty($_GET['forum']))
 {
 
-    ?>
+?>
+
 <div align="center">
 <table>
 <tr>
-<td width="100">Forum</td>
+<td width="100"><strong>Forum</strong></td>
 <td width="200"></td>
-<td width="50">Topics</td>
-<td width="50">Posts</td>
-<td width="200">Last Post</td>
+<td width="50"><strong>Topics</strong></td>
+<td width="50"><strong>Posts</strong></td>
+<td width="200"><strong>Last Post</strong></td>
 </tr>
 </div>
+
         <?php
         $stmt = $dbh->query('SELECT * FROM forum_main');
         $stmt->execute();
@@ -31,7 +34,6 @@ if (empty($_GET['forum']))
         {
             echo 'No forums created yet!';
         }
-
 	//create a foreach to gather our fetch array.
 	foreach ($forum_main as $forum)
 	{
@@ -41,7 +43,7 @@ if (empty($_GET['forum']))
                 $lastpost = $forum['lastpost'];
                 $totaltopics = $forums->totalTopics($forumid);
                 $totalreplies = $forums->totalReplies($forumid);
-		echo '<tr><td><a href="forum.php?forum='.$forumid.'">' .$forum_name . '</a></td><td><i>' . $forum_desc . '</i></td><td>'.$totaltopics.'</td><td>'.$totalreplies.'</div></td><td><i>last post by ' .$lastpost. '</i></td>';
+		echo '<tr><td><a href="forum.php?forum='.$forumid.'">' .$forum_name . '</a></td><td>' . $forum_desc . '</td><td>'.$totaltopics.'</td><td>'.$totalreplies.'</div></td><td>last post by ' .$lastpost. '</td>';
 
 	} //end foreach()
         echo '</tr></table>';
@@ -50,6 +52,20 @@ if (empty($_GET['forum']))
 
 if ($_GET['forum'])
 {
+
+?>
+
+<div align="center">
+<table>
+<tr>
+<td width="100"><strong>Forum</strong></td>
+<td width="200"></td>
+<td width="50"><strong>Posts</strong></td>
+<td width="200"><strong>Last Post</strong></td>
+</tr>
+</div>
+
+        <?php
 	//first we check to see if there are any topics created.
 	$stmt = $dbh->prepare('SELECT id,title,starter,lastreply FROM topics WHERE ' . 'forum=:forum ');
 	$stmt->bindParam('forum', $_GET['forum']);
@@ -62,17 +78,18 @@ if ($_GET['forum'])
 	}
 	else
 	{
-        	foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $topics)
+        	foreach ($topics as $topic)
                 {
-			$topic_id = $topics['id']; //id of topic.
-			$topic_title = $topics['title']; //topic title.
-			$topic_starter = $topics['starter']; //topic starter.
-			$topic_lastreply = $topics['lastreply']; //last replied user
-			echo '<a href="topic.php?id='.$topic_id.'">' .$topic_title . '<br />';
+			$topic_id = $topic['id']; //id of topic.
+			$topic_title = $topic['title']; //topic title.
+			$topic_starter = $topic['starter']; //topic starter.
+			$topic_lastreply = $topic['lastreply']; //last replied user
+			echo '<tr><td><a href="topic.php?id='.$topic_id.'">' .$topic_title . '</td><td></td><td>0</td><td>last post by</td>';
 
                 } //end foreach()
-
-                echo '<br /><br /><a href="addtopic.php?forum='.htmlspecialchars($_GET['forum'],ENT_QUOTES).'">Add Topic</a>';
+		
+                echo '</tr></table>';
+                echo '<br /><a href="addtopic.php?forum='.htmlspecialchars($_GET['forum'],ENT_QUOTES).'">Post new topic</a> - <a href="forum.php">Forum Main</a>';
 
 	} //end else()
 
